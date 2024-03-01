0121679cff1d7605be31ba4f091a96c2be7b2705
“23nith” <zenithmanuel@gmail.comgit config --global user.email ZENITHMANUEL@GMAIL.com>
2024-03-01 20:21:39 +0800

    Add data.js

---
+++ src/data.js
@@ -0,0 +1,40 @@
+import Thumbnail1 from './images/blog1.jpg'
+import Thumbnail2 from './images/blog2.jpg'
+import Thumbnail3 from './images/blog3.jpg'
+import Thumbnail4 from './images/blog4.jpg'
+
+export const DUMMY_POSTS = [
+    {
+      id: '1',
+      thumbnail: Thumbnail1,
+      category: 'education',
+      title: 'This is the title of the very first post on this blog.',
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi earum sit consectetur autem similique rem natus officia deserunt odit non neque impedit expedita blanditiis ad magni voluptas sequi ipsa, iste, asperiores at. Similique tenetur beatae qui odit, quia pariatur quis, aliquam mollitia, dolorum voluptate minus maiores voluptatum excepturi assumenda sequi?",
+      authorID: 3
+    },
+    {
+      id: '2',
+      thumbnail: Thumbnail2,
+      category: 'science',
+      title: 'This is the title of the very second post on this blog.',
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi earum sit consectetur autem similique rem natus officia deserunt odit non neque impedit expedita blanditiis ad magni voluptas sequi ipsa, iste, asperiores at. Similique tenetur beatae qui odit, quia pariatur quis, aliquam mollitia, dolorum voluptate minus maiores voluptatum excepturi assumenda sequi?",
+      authorID: 1
+    },
+    {
+      id: '3',
+      thumbnail: Thumbnail3,
+      category: 'weather',
+      title: 'This is the title of the very third post on this blog.',
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi earum sit consectetur autem similique rem natus officia deserunt odit non neque impedit expedita blanditiis ad magni voluptas sequi ipsa, iste, asperiores at. Similique tenetur beatae qui odit, quia pariatur quis, aliquam mollitia, dolorum voluptate minus maiores voluptatum excepturi assumenda sequi?",
+      authorID: 13
+    },
+    {
+      id: '4',
+      thumbnail: Thumbnail4,
+      category: 'farming',
+      title: 'This is the title of the very fourth post on this blog.',
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi earum sit consectetur autem similique rem natus officia deserunt odit non neque impedit expedita blanditiis ad magni voluptas sequi ipsa, iste, asperiores at. Similique tenetur beatae qui odit, quia pariatur quis, aliquam mollitia, dolorum voluptate minus maiores voluptatum excepturi assumenda sequi?",
+      authorID: 11
+    },
+  ]
+  
